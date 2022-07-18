7dfa1afdbab7edf838c0334e4efc1dc433a23ebc
Daniel Franchi <daniel2mf@yahoo.com.br>
2022-07-17 22:52:45 -0300

    Login screen interface development

---
+++ loginScreen/loginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginScreenApp.swift
+//  loginScreen
+//
+//  Created by mucci on 17/07/22.
+//
+
+import SwiftUI
+
+@main
+struct loginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
